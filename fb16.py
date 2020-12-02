82b4860d76d1bff6c8c3869709f67cb18387a8a5
Hoàng Cao Minh <35196884+hoangcaominh@users.noreply.github.com>
2020-12-02 17:55:37 +0700

    Rename file and function name

--- fb16.py
+++ fb16.py
@@ -2,9 +2,7 @@ if __name__ == '__main__':
     print('This is a module for encoding/decoding command lines made initially for tr.')
     exit()
 
-block = 4
-
-def decode(val):
+def fb16decode(val=0, block=4):
     if val == 0:
         return ''
 
@@ -12,7 +10,7 @@ def decode(val):
     str = ('0' if len(str) % 2 != 0 else '') + str
     return ''.join([''.join([chr(127 - int(str[i:i + block * 2][::-1][j:j + 2], 16)) for j in range(0, block * 2, 2)]) for i in range(0, len(str), block * 2)]).strip('\0')
 
-def encode(str):
+def fb16encode(str='', block=4):
     if str == '':
         return 0
         
@@ -20,7 +18,7 @@ def encode(str):
     return int(''.join([''.join([format(127 - ord(c), '02x') for c in str[i:i + block]])[::-1] for i in range(0, len(str), block)]), 16)
 
 '''
-Flipped-Block-Hex Encoder (fb16endocer)
+Flipped-Block-Hexadecimal Encoder (fb16)
 Algorithm:
 Append null character until len(str) % block == 0
 For each substr of block characters:
